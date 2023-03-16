a312bbd66d8d08ead080ccf42ea3714ef7eff165
Jenny Rafferty <raffertyjenny@gmail.com>
2023-03-16 16:13:07 +0000

    Getting Older, Getting Unhealthier

---  __tests__/pet.test.js
+++  __tests__/pet.test.js
@@ -14,13 +14,29 @@ describe('constructor', () => {
     });
   });
   describe('constructor', () => {
-    it('has a initial age of 0', () => {
+    it('has an initial age of 0', () => {
       const pet = new Pet('Fido');
   
       expect(pet.age).toEqual(0);
     });
   });
+
+    describe('constructor', () => {
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
   
+      expect(pet.hunger).toEqual(0);
+    });
+  });
+
+    describe('constructor', () => {
+    it('has an initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.fitness).toEqual(10);
+    });
+  });
+
   describe('growUp', () => {
     it('increments the age by 1', () => {
       const pet = new Pet('Fido');
@@ -29,4 +45,18 @@ describe('constructor', () => {
   
       expect(pet.age).toEqual(1);
     });
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+
+        pet.growUp();
+    
+        expect(pet.hunger).toEqual(5);
+      });
+      it('decreases the fitness by 3', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.fitness).toEqual(7);
+      });
   });
